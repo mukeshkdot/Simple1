28953f8344388d663820a372466d705de501830a
DST-5 <DST-5@DESKTOP-1J9RFD0>
2021-03-15 23:55:20 +0530

    change in home

--- WebApplication14/Controllers/HomeController.vb
+++ WebApplication14/Controllers/HomeController.vb
@@ -6,7 +6,7 @@
 	End Function
 
 	Function About() As ActionResult
-		ViewData("Message") = "Your application description page. Testing jjj"
+		ViewData("Message") = "Your application description page. Testing "
 
 		Return View()
 	End Function
